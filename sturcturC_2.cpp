7260bd9ccc4c48e2b5979932c39a330fb684751d
wesclicdev <wesclicdev@gmail.com>
2024-04-02 16:41:20 +0700

    merincikan alamat mahasiswa nama kota

--- sturcturC_2.cpp
+++ sturcturC_2.cpp
@@ -23,8 +23,10 @@ int main() {
     cout << "Alamat Mahasiswa : ";
     cout << "/t Nama Desa : ";
     cin >> mhs.alamat.desa;
+    cout << "/t Nama Kota : ";
+    cin >> mhs.alamat.kota;
 
-    
+
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
 
