4dc36b912e33a69c838beb3422dd9722cf8a69a5
wesclicdev <wesclicdev@gmail.com>
2024-04-02 16:40:02 +0700

    merincikan alamat mahasiswa

--- sturcturC_2.cpp
+++ sturcturC_2.cpp
@@ -12,3 +12,26 @@ struct Mahasiswa {
     DetailAlamat alamat;
     string umur;
 };
+
+int main() {
+    Mahasiswa mhs;
+    cout << "Nomor Mahasiswa : ";
+    cin >> mhs.nim;
+    cout << "Nama Mahasiswa : ";
+    cin >> mhs.nama;
+
+    cout << "Alamat Mahasiswa : ";
+    cout << "/t Nama Desa : ";
+    cin >> mhs.alamat.desa;
+
+    
+    cout << "Umur Mahasiswa : ";
+    cin >> mhs.umur;
+
+    cout << endl;
+    cout << "/n NIM : " <<mhs.nim;
+    cout << "/n Nama : " <<mhs.nama;
+    cout << ".n Alamat : " <<mhs.alamat;
+    cout << "/n Umur : " <<mhs.umur;
+
+}
